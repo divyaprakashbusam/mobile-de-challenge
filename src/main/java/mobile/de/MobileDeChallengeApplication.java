e4f1107a89bdaa8f8e023b81c57b4ceaa3749e1e
190030233 <190030233@kluniversity.in>
2022-06-24 16:28:17 +0530

    first commit

---
+++ src/main/java/mobile/de/MobileDeChallengeApplication.java
@@ -0,0 +1,13 @@
+package mobile.de;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileDeChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileDeChallengeApplication.class, args);
+	}
+
+}
